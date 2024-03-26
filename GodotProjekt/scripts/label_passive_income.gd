965ca802b5dad07306193c95856b7bc14e5eaa91
Tob6 <Tobias-Weisheit0@web.de>
2024-03-26 15:44:10 +0100

    refactoring number-suffix code; applying it to other number text fields

--- GodotProjekt/scripts/label_passive_income.gd
+++ GodotProjekt/scripts/label_passive_income.gd
@@ -2,4 +2,5 @@ extends Label
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	set_text(str(ScoreNode.passive_income) + "🍎 per second");
+	var passive_income = ScoreNode.passive_income;
+	set_text(NumSuffix.number_with_suffix(passive_income) + "🍎 per second");
